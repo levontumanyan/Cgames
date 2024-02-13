4577b1089db565a5438b86eec941e8e0dcdc2d5f
itfibonacci <levon.tumanyan@outlook.com>
2024-02-13 11:48:06 -0500

    bottom bar fixed

--- blockbreaker/src/game.c
+++ blockbreaker/src/game.c
@@ -148,7 +148,7 @@ void check_for_collision_with_bar(Ball *ball, Bar *bottom_bar) {
 	} else if ((ball->body.x >= bottom_bar->body[0].x && ball->body.x < bottom_bar->body[0].x + bottom_bar->length / 2) && ball->body.y == bottom_bar->body[0].y - 1)
 	{
 		ball->direction = 2;
-	} else if ((ball->body.x > bottom_bar->body[0].x + bottom_bar->length / 2) && ball->body.y == bottom_bar->body[0].y - 1) {
+	} else if ((ball->body.x > bottom_bar->body[0].x + bottom_bar->length / 2) && (ball->body.x <= bottom_bar->body[0].x + bottom_bar->length) && (ball->body.y == bottom_bar->body[0].y - 1)) {
 		ball->direction = 3;
 	}
 }
