af781e2aab0ae258aa322f1d22327a34288ffcdf
itfibonacci <levon.tumanyan@outlook.com>
2024-02-22 17:11:08 -0500

    check winning condition

--- tictactoe/src/game.c
+++ tictactoe/src/game.c
@@ -19,11 +19,13 @@ Game *start_game() {
 		return NULL;
 	}
 	game->game_over = 0;
-	// turns this into a random function
-	game->turn = 0;
+	// turn this into a random function
+	game->turn = 1;
+
+	// fill up the game board with 2s each time a user clicks on a box that cell's value in the board will become either 0 or 1. 0 for 0, 1 for x
 	for (unsigned char i = 0; i < SQUARE_DIMENSION; i++) {
 		for (unsigned char j = 0; j < SQUARE_DIMENSION; j++) {
-			game->board[i][j] = 0;
+			game->board[i][j] = 2;
 		}
 	}
 	return game;
@@ -34,6 +36,21 @@ void monitor_game(WINDOW *win, Game *game) {
 		draw_board(win);
 		if (game->game_over == 1) {
 			break;
+		}
+	}
+}
+
+unsigned char check_winning_condition(Game *game) {
+	// idea will be take the board and check for a line or diagonal full of 1s or 2s
+	// return 1 if there is a winner, 0 otherwise
+	// check rows for the sum is my idea
+	unsigned char row_sum = 0;
+	for (unsigned char i = 0; i < SQUARE_DIMENSION; i++) {
+		for (unsigned char j = 0; j < SQUARE_DIMENSION; j++) {
+			row_sum += game->board[i][j];
+		}
+		if (row_sum == SQUARE_DIMENSION || row_sum == 0) {
+			return 1;
 		}
 	}
 }
